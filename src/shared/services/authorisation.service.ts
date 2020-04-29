946eed46b0414ea8bf6a37ba613dd650f0f84f88
Kirill Pakhtusov <pak.kiruha@yandex.ru>
2020-04-29 11:20:51 +0300

    fix hhtps connection

--- src/shared/services/authorisation.service.ts
+++ src/shared/services/authorisation.service.ts
@@ -8,7 +8,7 @@ import {Router} from "@angular/router";
   providedIn: 'root'
 })
 export class AuthorisationService {
-  public uri = 'http://ec2-13-59-245-120.us-east-2.compute.amazonaws.com:5000';
+  public uri = 'https://edupulse.ru';
   public token;
   constructor(private http: HttpClient, public router: Router) { }
 
--- src/shared/services/client.services.ts
+++ src/shared/services/client.services.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ClientService {
-  public uri = 'http://ec2-13-59-245-120.us-east-2.compute.amazonaws.com:5000/api/Client';
+  public uri = 'https://edupulse.ru/api/Client';
   constructor(private http: HttpClient){ }
 
   getClients(): Observable<any> {
